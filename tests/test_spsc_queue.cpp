904baf8b2cde356ce3475bc8e5a0a0bd489b8bb8
Maksymilian Suchecki <maks.suchecki@gmail.com>
2026-08-23 19:37:01 +0200

    test: add multi-threaded producer/consumer test for SpscQueue

--- tests/test_spsc_queue.cpp
+++ tests/test_spsc_queue.cpp
@@ -1,11 +1,13 @@
 #include <gtest/gtest.h>
+#include <thread>
+#include <vector>
 
 #include "ome/spsc_queue.hpp"
 
 using namespace ome;
 
-TEST(SpscQueue, PopReturnsElementsInFifoOrder) {
-    SpscQueue<int, 8> spsc;
+TEST(SpscQueue, SingleThread){
+    ome::SpscQueue<int, 8> spsc;
 
     spsc.push(5);
     spsc.push(10);
@@ -21,35 +23,63 @@ TEST(SpscQueue, PopReturnsElementsInFifoOrder) {
     EXPECT_EQ(third, 20);
 }
 
-TEST(SpscQueue, FullPushReturnsFalse) {
-    SpscQueue<int, 4> spsc;
+TEST(SpscQueue, FullPushReturnsFalse){
+    ome::SpscQueue<int, 4> spsc;
 
     EXPECT_TRUE(spsc.push(5));
     EXPECT_TRUE(spsc.push(10));
     EXPECT_TRUE(spsc.push(20));
     EXPECT_TRUE(spsc.push(40));
 
-    EXPECT_FALSE(spsc.push(999)); // bufor pełny, piąty push się nie uda
+    EXPECT_FALSE(spsc.push(99));
 }
 
-TEST(SpscQueue, EmptyPopReturnsFalse) {
+TEST(SpscQueue, EmptyPopReturnsFalse){
     SpscQueue<int, 4> spsc;
-
+    
     int value;
     EXPECT_FALSE(spsc.pop(value));
 }
-
-TEST(SpscQueue, PopAfterFullFreesSlotForNextPush) {
-    SpscQueue<int, 4> spsc;
-
+TEST(SpscQueue, PopAfterFullFreesSlotsForNextPush){
+    ome::SpscQueue<int, 4> spsc;
     spsc.push(1);
     spsc.push(2);
     spsc.push(3);
-    spsc.push(4); // pełne
+    spsc.push(4);
 
     int value;
     ASSERT_TRUE(spsc.pop(value));
     EXPECT_EQ(value, 1);
 
-    EXPECT_TRUE(spsc.push(5)); // teraz jest miejsce
+    EXPECT_TRUE(spsc.push(5));
+}
+
+TEST(SpscQueue, ConcurrentProducerConsumerPreserverOrder){
+    constexpr int kCount = 100000;
+    SpscQueue<int, 1024> spsc;
+    std::vector<int> consumed;
+    consumed.reserve(kCount);
+
+    std::thread producer([&spsc]() {
+        for (int i = 0; i < kCount; i++){
+            while (!spsc.push(i)){
+                
+            }
+        }
+    });
+    std::thread consumer([&spsc, &consumed]() {
+        int value;
+        for (int i = 0; i < kCount; i++){
+            while (!spsc.pop(value)){
+
+            }
+            consumed.push_back(value);
+        }
+    });
+    producer.join();
+    consumer.join();
+    ASSERT_EQ(consumed.size(), kCount);
+    for (int i = 0; i < kCount; i++){
+        EXPECT_EQ(consumed[i], i);
+    }
 }
